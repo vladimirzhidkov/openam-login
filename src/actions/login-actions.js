67edac77871a9493a9a8a27bf28118faf0f5d4f3
vlad <vladimir.zhidkov@icloud.com>
2016-01-15 18:35:58 -0700

    initial commit

---
+++ src/actions/login-actions.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//import dispatcher from '../dispatcher';
+//import ActionTypes from '../constants/action-types';
+
+
+let rest = {
+    //host: 'http://localhost:8080/OpenAM-13.0.0-SNAPSHOT_20151116',
+    host: 'http://openam.mycomputer.com:8080/OpenAM-13.0.0-SNAPSHOT_20151116',
+    get: function(path) {
+        let url = this.host + path;
+        return new Promise(function resolver(resolve, reject) {
+            $.ajax({url: url, success: resolve, error: reject});
+        });
+    },
+    post: function(path, data) {
+        let url = this.host + path;
+        return new Promise(function resolver(resolve, reject) {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                contentType: 'application/json',
+                data: data,
+                dataType: 'json',
+                success: (data, textStatus, jqXHR)=>resolve(data),
+                error: res=>reject(res.status)
+                //error: res=>console.log(res.status)
+            });
+        });
+    }
+};
+
+class LoginActions {
+
+    static authenticate(callback){
+
+        let success = function(data){
+            console.log(data);
+        };
+        let error = function(status){
+            console.log(status);
+        };
+
+        rest.post('/json/authenticate')
+            .then(success)
+            .catch(error);
+    }
+}
+export default LoginActions;
--- src/components/login-page.react.js
+++ src/components/login-page.react.js
@@ -4,51 +4,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router';
 
+import LoginActions from '../actions/login-actions';
+
 class LoginPage extends React.Component {
     constructor() {
         super();
         this.state = {login:null, password:null};
     }
 
+    componentWillMount(){
+        LoginActions.authenticate((data)=>console.log(data));
+    }
+
     render() {
 
-        let styleForm = {borderRadius:10, backgroundColor:'#f2f2f2', padding:10};
-        let styleInput = {margin:5, borderRadius:6 };
-        let styleLoginBtn = {margin:10, color:'#fff', backgroundColor:'#29C9FF'};
         let styleBanner = {textAlign:'center', backgroundColor:'rgba(242, 242, 242, 0.85)', padding:10, border:'2px solid #29abe1'};
+        let styleLoginForm = {width:300, borderRadius:10, backgroundColor:'#f2f2f2', padding:30, display:'inline-block'};
+        let styleInput = {borderRadius:6, margin:'10px 0px'};
+        let styleLoginBtn = {color:'#fff', backgroundColor:'#29C9FF', margin:'10px 0px'};
 
         return(
             <div className="container-fluid">
-                <div style={styleBanner} className="row">
+
+                {/* top banner */}
+                <div className="row" style={styleBanner}>
                     <p><img style={{maxHeight:90, maxWidth:500}} src="img/banner.gif"/></p>
                     <span>Login to access the Portal</span>
                 </div>
 
-                <div className="row row-margin-top-60">
-                    <div style={styleForm} className="form-inline col-sm-offset-5 col-sm-2 text-center">
+                {/* login form */}
+                <div className="row row-margin-top-60 text-center">
+                    <div className="form text-center" style={styleLoginForm}>
                         <p><img src="img/utah-id-logo.png"/></p>
+
                         <input style={styleInput}
-                               size={19}
-                               className="form-control"
+                               size={20}
+                               className="form-control input-lg"
                                placeholder="Login"
                                autoFocus={true}
-                               onChange={event=>this.setState({login:event.target.value})}/><br/>
+                               onChange={event=>this.setState({login:event.target.value})}/>
+
                         <input style={styleInput}
-                               size={19}
-                               className="form-control"
+                               size={20}
+                               className="form-control input-lg"
                                type="password"
                                placeholder="Password"
-                               onChange={event=>this.setState({password:event.target.value})}/><br/>
+                               onChange={event=>this.setState({password:event.target.value})}/>
+
                         <button style={styleLoginBtn}
                                 type="button"
-                                className="btn"
+                                className="btn btn-lg"
                                 onClick={event=>{
                                     event.preventDefault();
                                     this.onLoginButtonClick();
-                            }}
-                            >Login</button><br/>
-                        <a href="">Forgot Password</a><br/>
-                        <a href="">Create Account</a>
+                                }}>Login</button>
+
+                        <p><a href="">Forgot Password</a></p>
+                        <p><a href="">Create Account</a></p>
                     </div>
                 </div>
             </div>
